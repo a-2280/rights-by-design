9fb8811b05600080a85f532ac199a2c1e1d4c104
Calvin <calvin@Calvins-MacBook-Air.local>
2026-01-27 20:02:00 -0800

    fix for mobile chrome turning possible links into actual links

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,12 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
+const React = require("react")
+
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+    setHtmlAttributes({ lang: `en` })
+    setHeadComponents([
+        React.createElement("meta", {
+            key: "format-detection",
+            name: "format-detection",
+            content: "telephone=no, date=no, email=no, address=no"
+        })
+    ])
 }
