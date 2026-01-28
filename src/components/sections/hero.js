27d385d9f45faea2c75501a8e23d6241694c53b2
Calvin <calvin@Calvins-MacBook-Air.local>
2026-01-27 19:38:53 -0800

    added 100dvh to hero instead of 100vh

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -4,7 +4,7 @@ import Header from "../header"
 
 export default function Hero() {
     return (
-        <section className='h-100vh flex flex-col space-between bg-black text-white'>
+        <section className='h-100dvh flex flex-col space-between bg-black text-white'>
             <Header />
             <div className='ratio-2-1 pos-rel h-100 overflow'>
                 <iframe className='bg-image' src='https://player.vimeo.com/video/1124381270?background=1&autoplay=1&loop=1&muted=1'></iframe>
